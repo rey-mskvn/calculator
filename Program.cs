2e7b31f0b00c58394e56d25775296af952a6197c
rey-mskvn <rey-mskvn@yandex.ru>
2026-04-09 18:22:08 +0500

    FINAL

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+class Program
+{
+    static int Add(int a, int b)
+    { return a + b; }
+
+    static int Subtract(int a, int b)
+    { return a - b; }
+
+    static int Multiply(int a, int b)
+    { return a * b; }
+
+    static void Main(string[] args)
+{
+    // VERSION: 2.0.0-FINAL
+    // Здесь будет логика калькулятора
+    
+    Console.Write("Введите первое число: ");
+    string input1 = Console.ReadLine();
+    int num1 = int.Parse(input1);
+    
+    Console.Write("Введите второе число: ");
+    string input2 = Console.ReadLine();
+    int num2 = int.Parse(input2);
+    
+    Console.Write("Выберите операцию (+, -, *): ");
+    string operation = Console.ReadLine();
+    
+    int result = 0;
+    
+    if (operation == "+")
+        result = num1 + num2;
+    else if (operation == "-")
+        result = num1 - num2;
+    else if (operation == "*")
+        result = num1 * num2;
+    
+    Console.WriteLine($"Результат: {result}");
+}
+}
